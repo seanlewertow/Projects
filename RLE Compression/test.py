67078c434e5f56d68abf1cd061847cfeaf7f25e9
Sean Lewertow <116752300+seanlewertow@users.noreply.github.com>
2023-10-01 22:01:10 -0400

    Encodes and decodes RLE data with image manipulation

---
+++ RLE Compression/test.py
@@ -0,0 +1,6 @@
+from rle_program import *
+
+print(count_runs([2, 2, 3, 4, 4, 4]))
+print(encode_rle([15, 15, 15, 4, 4, 4, 4, 4, 4]))
+print(get_decoded_length([3, 15, 6, 4]))
+print(decode_rle([3, 15, 6, 4]))
